41d70b45ebe9e1f4dbd5203392748c21143446cc
superpanash <110617003+superpanash@users.noreply.github.com>
2023-06-29 15:19:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
 var nose_x=0;
 var nose_y=0;
 
-
-
-
-
 function preload(){
-mustache=loadImage="https://i.postimg.cc/3x3QzSGq/m.png";
+mustache=loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
 }
 
 function setup(){
@@ -14,19 +10,18 @@ canvas=createCanvas(350,350);
 canvas.center();
 
 video=createCapture(VIDEO);
-video.size(350,350)
+video.size(350,350);
 video.hide();
 
-posenet=ml5.poseNet(video,modelLoaded)
+posenet=ml5.poseNet(video,modelLoaded);
 
-posenet.on("pose",gotPoses)
+posenet.on("pose",gotPoses);
 
 }
 
-
 function draw(){
 image(video,0,0,350,350);
-image(mustache ,nose_x,nose_y,30,30)
+image(mustache,nose_x,nose_y,30,30)
 }
 
 function snapshot(){
@@ -41,8 +36,8 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
-        nose_x=results[0].pose.nose.x-20;
-        nose_y=results[0].pose.nose.y-20;
+        nose_x=results[0].pose.nose.x-5;
+        nose_y=results[0].pose.nose.y-5;
         console.log("Nose x ="+nose_x);
         console.log("nose y ="+nose_y);
     }
